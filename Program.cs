2b096deb510aaedf2deb1dcd75f7ccddfe911958
Madhur82954 <itzmadhurverma@gmail.com>
2023-05-19 12:06:29 +0530

    [Madhur] - Employee Presence Marked

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EmployeeWageComputationProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC1 - Employee present or absent
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+
+            int empcheck = random.Next(0,2);
+
+            if (empcheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
