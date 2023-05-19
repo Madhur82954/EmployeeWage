502ae0b28885f5d1be03c6e74c95f8736b410073
Madhur82954 <itzmadhurverma@gmail.com>
2023-05-19 12:20:55 +0530

    [Madhur] -Employee Wage Calculated

--- Program.cs
+++ Program.cs
@@ -6,20 +6,41 @@ namespace EmployeeWageComputationProblem
     {
         static void Main(string[] args)
         {
-            //UC1 - Employee present or absent
+            /* //UC1 - Employee present or absent
+             int IS_FULL_TIME = 1;
+             Random random = new Random();
+
+             int empcheck = random.Next(0,2);
+
+             if (empcheck == IS_FULL_TIME)
+             {
+                 Console.WriteLine("Employee is present");
+             }
+             else
+             {
+                 Console.WriteLine("Employee is absent");
+             } */
+            //UC2-EmployeeWage
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+
+            int emphrs = 0;
+            int empwage = 0;
+
             Random random = new Random();
 
             int empcheck = random.Next(0,2);
-
-            if (empcheck == IS_FULL_TIME)
+            if(empcheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is present");
+                emphrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is absent");
+                emphrs = 0;
             }
+            empwage = emphrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee wage = " + empwage);
+
         }
     }
 }
