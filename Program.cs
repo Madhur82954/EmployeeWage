9c49504a2f247cf70fe5cf9ff39670134a6f8a51
Madhur82954 <itzmadhurverma@gmail.com>
2023-06-06 13:10:41 +0530

    [Madhur] - Multiple Companies Using Interface

--- Program.cs
+++ Program.cs
@@ -3,161 +3,166 @@ using System;
 
 namespace EmployeeWageComputationProblem
 {
-        internal class Employee
+    internal interface IComputeWage
+    {
+        void AddCompany(string name, int wagePerHour, int workingHoursPerMonth, int workingDaysPerMonth);
+        void CalculateWages();
+    }
+    internal class Employee
+    {
+        private bool isPresent;
+        private bool isPartTime;
+        private int wagePerHour;
+        private int workingHours;
+        private int totalWage;
+
+        public Employee(bool isPresent, bool isPartTime, int wagePerHour, int workingHours)
         {
-            private bool isPresent;
-            private bool isPartTime;
-            private int wagePerHour;
-            private int workingHours;
-            private int totalWage;
+            this.isPresent = isPresent;
+            this.isPartTime = isPartTime;
+            this.wagePerHour = wagePerHour;
+            this.workingHours = workingHours;
+            this.totalWage = 0;
+        }
 
-            public Employee(bool isPresent, bool isPartTime, int wagePerHour, int workingHours)
+        public bool IsPresent()
+        {
+            return isPresent;
+        }
+
+        public bool IsPartTime()
+        {
+            return isPartTime;
+        }
+
+        public int CalculateDailyWage()
+        {
+            return wagePerHour * workingHours;
+        }
+
+        public void CalculateMonthlyWage(int totalWorkingHours, int totalWorkingDays)
+        {
+            int workingHoursCounter = 0;
+            int workingDaysCounter = 0;
+
+            while (workingHoursCounter < totalWorkingHours && workingDaysCounter < totalWorkingDays)
             {
-                this.isPresent = isPresent;
-                this.isPartTime = isPartTime;
-                this.wagePerHour = wagePerHour;
-                this.workingHours = workingHours;
-                this.totalWage = 0;
-            }
-
-            public bool IsPresent()
-            {
-                return isPresent;
-            }
-
-            public bool IsPartTime()
-            {
-                return isPartTime;
-            }
-
-            public int CalculateDailyWage()
-            {
-                return wagePerHour * workingHours;
-            }
-
-            public void CalculateMonthlyWage(int totalWorkingHours, int totalWorkingDays)
-            {
-                int workingHoursCounter = 0;
-                int workingDaysCounter = 0;
-
-                while (workingHoursCounter < totalWorkingHours && workingDaysCounter < totalWorkingDays)
+                if (IsPresent())
                 {
-                    if (IsPresent())
-                    {
-                        workingDaysCounter++;
-                        int dailyWage = CalculateDailyWage();
-                        totalWage += dailyWage;
-                        workingHoursCounter += workingHours;
-                    }
+                    workingDaysCounter++;
+                    int dailyWage = CalculateDailyWage();
+                    totalWage += dailyWage;
+                    workingHoursCounter += workingHours;
                 }
-            }
-
-            public int GetTotalWage()
-            {
-                return totalWage;
             }
         }
 
-        internal class CompanyEmpWage
+        public int GetTotalWage()
         {
-            private string name;
-            private int wagePerHour;
-            private int workingHoursPerMonth;
-            private int workingDaysPerMonth;
+            return totalWage;
+        }
+    }
 
-            public CompanyEmpWage(string name, int wagePerHour, int workingHoursPerMonth, int workingDaysPerMonth)
+    internal class CompanyEmpWage
+        {
+        private string name;
+        private int wagePerHour;
+        private int workingHoursPerMonth;
+        private int workingDaysPerMonth;
+
+        public CompanyEmpWage(string name, int wagePerHour, int workingHoursPerMonth, int workingDaysPerMonth)
+        {
+            this.name = name;
+            this.wagePerHour = wagePerHour;
+            this.workingHoursPerMonth = workingHoursPerMonth;
+            this.workingDaysPerMonth = workingDaysPerMonth;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public int GetWagePerHour()
+        {
+            return wagePerHour;
+        }
+
+        public int GetWorkingHoursPerMonth()
+        {
+            return workingHoursPerMonth;
+        }
+
+        public int GetWorkingDaysPerMonth()
+        {
+            return workingDaysPerMonth;
+        }
+    }
+
+    internal class EmpWageBuilder:IComputeWage
+    {
+        private CompanyEmpWage[] companies;
+        private int companyCount = 0;
+
+        public EmpWageBuilder()
+        {
+            companies = new CompanyEmpWage[2];
+            companyCount = 0;
+        }
+
+        public void AddCompany(string name, int wagePerHour, int workingHoursPerMonth, int workingDaysPerMonth)
+        {
+            if (companyCount < companies.Length)
             {
-                this.name = name;
-                this.wagePerHour = wagePerHour;
-                this.workingHoursPerMonth = workingHoursPerMonth;
-                this.workingDaysPerMonth = workingDaysPerMonth;
+                companies[companyCount] = new CompanyEmpWage(name, wagePerHour, workingHoursPerMonth, workingDaysPerMonth);
+                companyCount++;
             }
-
-            public string GetName()
+            else
             {
-                return name;
-            }
-
-            public int GetWagePerHour()
-            {
-                return wagePerHour;
-            }
-
-            public int GetWorkingHoursPerMonth()
-            {
-                return workingHoursPerMonth;
-            }
-
-            public int GetWorkingDaysPerMonth()
-            {
-                return workingDaysPerMonth;
+                Console.WriteLine("Reached maximum limit of companies.");
             }
         }
 
-        internal class EmpWageBuilder
+        public void CalculateWages()
         {
-            private CompanyEmpWage[] companies;
-
-            public EmpWageBuilder()
+            for (int i = 0; i < companyCount; i++)
             {
-                companies = new CompanyEmpWage[2]; // Assuming 2 companies for demonstration
-            }
+                Employee employee = new Employee(true, false, companies[i].GetWagePerHour(), companies[i].GetWorkingHoursPerMonth());
 
-            public void AddCompany(string name, int wagePerHour, int workingHoursPerMonth, int workingDaysPerMonth)
-            {
-                for (int i = 0; i < companies.Length; i++)
+                if (employee.IsPresent())
                 {
-                    if (companies[i] == null)
+                    int dailyWage = employee.CalculateDailyWage();
+                    Console.WriteLine("Employee is Present.");
+
+                    switch (employee.IsPartTime())
                     {
-                        companies[i] = new CompanyEmpWage(name, wagePerHour, workingHoursPerMonth, workingDaysPerMonth);
-                        break;
+                        case true:
+                            Console.WriteLine($"Part-Time Employee. Daily Wage: {dailyWage}");
+                            break;
+                        case false:
+                            Console.WriteLine($"Full-Time Employee. Daily Wage: {dailyWage}");
+                            break;
                     }
+
+                    employee.CalculateMonthlyWage(companies[i].GetWorkingHoursPerMonth(), companies[i].GetWorkingDaysPerMonth());
+                    int monthlyWage = employee.GetTotalWage();
+                    Console.WriteLine($"Monthly Wage for {companies[i].GetName()}: {monthlyWage}");
                 }
-            }
-
-            public void CalculateWages()
-            {
-                for (int i = 0; i < companies.Length; i++)
+                else
                 {
-                    if (companies[i] != null)
-                    {
-                        Employee employee = new Employee(true, false, companies[i].GetWagePerHour(), companies[i].GetWorkingHoursPerMonth());
-
-                        if (employee.IsPresent())
-                        {
-                            int dailyWage = employee.CalculateDailyWage();
-                            Console.WriteLine("Employee is Present.");
-
-                            switch (employee.IsPartTime())
-                            {
-                                case true:
-                                    Console.WriteLine($"Part-Time Employee. Daily Wage: {dailyWage}");
-                                    break;
-                                case false:
-                                    Console.WriteLine($"Full-Time Employee. Daily Wage: {dailyWage}");
-                                    break;
-                            }
-
-                            employee.CalculateMonthlyWage(companies[i].GetWorkingHoursPerMonth(), companies[i].GetWorkingDaysPerMonth());
-                            int monthlyWage = employee.GetTotalWage();
-                            Console.WriteLine($"Monthly Wage for {companies[i].GetName()}: {monthlyWage}");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Employee is Absent");
-                        }
-                    }
+                    Console.WriteLine("Employee is Absent");
                 }
             }
         }
     }
-    internal class Program
-    {
+}
+internal class Program
+{
         static void Main(string[] args)
         {
-            EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+            IComputeWage empWageBuilder = new EmpWageBuilder();
             empWageBuilder.AddCompany("dMart", 20, 2, 10); 
             empWageBuilder.AddCompany("Reliance", 10, 4, 20); 
             empWageBuilder.CalculateWages();
-        }
+        } 
 }
